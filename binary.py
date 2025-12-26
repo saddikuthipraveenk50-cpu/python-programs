034f3087c743583f078b90d1b047416401a6409a
saddikuthipraveenk50-cpu <saddikuthipraveenk50@gmail.com>
2025-12-26 15:10:09 +0530

    Create binary.py

---
+++ binary.py
@@ -0,0 +1,13 @@
+def binary_search(arr, x):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+print(binary_search([1, 3, 5, 7], 5))
